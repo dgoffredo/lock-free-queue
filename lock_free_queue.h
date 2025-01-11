2ac37a650c919a3d3c1ce5a64f312eed65c4f508
David Goffredo <dmgoffredo@gmail.com>
2025-01-11 17:06:47 -0500

    It's still wrong.

--- lock_free_queue.h
+++ lock_free_queue.h
@@ -1,13 +1,91 @@
 
 #include <atomic>
+#include <cstdint>
 #include <optional>
 
-#include <iomanip> // TODO: no
-#include <iostream> // TODO: no
+// `TaggedPtr<T>` is a `T*`, but the least significant bit is used as a tag.
+// On 16-bit systems or larger (32-bit, 64-bit), pointer-aligned addresses
+// will always be a multiple of two, so the lowest bit of such addresses is
+// always zero.
+// That bit can be put to use. For example, in `Queue`, below, it's used to
+// mark a `Node` as being "busy."
+template <typename T>
+struct TaggedPtr {
+    std::uintptr_t raw;
 
-// TODO: This implementation does not provide any exception guarantees.
-// It could be modified to provide the strong exception guarantee, but I
-// haven't yet bothered.
+    T *ptr() const;
+    void ptr(T *new_value);
+    bool bit() const;
+    void bit(bool new_value);
+    
+    TaggedPtr();
+    TaggedPtr(T*, bool);
+    explicit TaggedPtr(std::uintptr_t);
+
+    T *operator->() const;
+};
+
+template <typename T>
+TaggedPtr<T>::TaggedPtr()
+: raw(0) {}
+
+template <typename T>
+TaggedPtr<T>::TaggedPtr(T *p, bool b)
+: raw(reinterpret_cast<std::uintptr_t>(p) | b) {}
+
+template <typename T>
+TaggedPtr<T>::TaggedPtr(std::uintptr_t raw)
+: raw(raw) {}
+
+template <typename T>
+T *TaggedPtr<T>::ptr() const {
+    return reinterpret_cast<T*>((raw >> 1) << 1);
+}
+
+template <typename T>
+void TaggedPtr<T>::ptr(T *new_value) {
+    *this = TaggedPtr(new_value, bit());
+}
+
+template <typename T>
+bool TaggedPtr<T>::bit() const {
+    return raw & 1;
+}
+
+template <typename T>
+void TaggedPtr<T>::bit(bool new_value) {
+    *this = TaggedPtr(ptr(), new_value);
+}
+
+template <typename T>
+T *TaggedPtr<T>::operator->() const {
+    return ptr();
+}
+
+template <typename T>
+class AtomicTaggedPtr {
+    std::atomic<std::uintptr_t> raw;
+
+public:
+    TaggedPtr<T> load() const;
+    void store(TaggedPtr<T>, std::memory_order = std::memory_order_seq_cst);
+    bool compare_exchange_weak(TaggedPtr<T>& expected, TaggedPtr<T> desired);
+};
+
+template <typename T>
+TaggedPtr<T> AtomicTaggedPtr<T>::load() const {
+    return TaggedPtr<T>(raw.load());
+}
+
+template <typename T>
+void AtomicTaggedPtr<T>::store(TaggedPtr<T> new_value, std::memory_order order) {
+    raw.store(new_value.raw, order);
+}
+
+template <typename T>
+bool AtomicTaggedPtr<T>::compare_exchange_weak(TaggedPtr<T>& expected, TaggedPtr<T> desired) {
+    return raw.compare_exchange_weak(expected.raw, desired.raw);
+}
 
 template <typename T>
 class Queue {
@@ -15,41 +93,45 @@ private:
     struct Node {
         union {
             T value;
-            Node *free_list_next;
         };
-        std::atomic<Node*> queue_next;
+        AtomicTaggedPtr<Node> next;
 
-        Node() {}
+        Node()
+        : next() {}
 
         ~Node() {}
     };
 
-    std::atomic<Node*> front;
-    std::atomic<Node*> back;
+    std::atomic<Node*> before_first;
+    std::atomic<Node*> last;
     std::atomic<Node*> free_list;
 
 public:
     Queue()
-    : front(nullptr)
-    , back(nullptr) {}
+    : before_first(new Node) // "dummy" node
+    , last(before_first.load())
+    , free_list(nullptr)
+    {}
 
     ~Queue() {
         Node *next;
 
         // Delete nodes in the queue.
-        for (Node *node = front.load(std::memory_order_relaxed); node; node = next) {
-            std::cout << "queue node has value: " << node->value << '\n';
-            next = node->queue_next.load(std::memory_order_relaxed);
-            std::cout << "queue node->queue_next is: " << std::hex << static_cast<const void*>(next) << '\n';
+        Node *node = before_first.load(std::memory_order_relaxed);
+        next = node->next.load().ptr();
+        // The first node is the "dummy" without a value, so don't call ~T().
+        delete node;
+        node = next;
+        while (node) {
+            next = node->next.load().ptr();
             node->value.~T();
             delete node;
+            node = next;
         }
 
         // Delete nodes in the free list.
-        for (Node *node = free_list; node; node = next) {
-            std::cout << "free list node is: " << std::hex << static_cast<const void*>(node) << '\n';
-            next = node->free_list_next;
-            std::cout << "free list next is: " << std::hex << static_cast<const void*>(next) << '\n';
+        for (Node *node = free_list.load(); node; node = next) {
+            next = node->next.load().ptr();
             delete node;
         }
     }
@@ -58,20 +140,31 @@ public:
     void push_back(Value&& value) {
         // Get a node from the free list, or otherwise allocate a new node.
         Node *node;
-        Node *new_free_list;
+        TaggedPtr<Node> next;
         do {
             node = free_list.load();
             if (!node) {
                 break;
             }
-            new_free_list = node->free_list_next;
-        } while (!free_list.compare_exchange_weak(node, new_free_list));
+            next = node->next.load();
+            // The `bit` is used to store whether the node is "busy."
+            // A node is busy if its value is being moved from or is being destroyed.
+            if (next.bit()) {
+                // The node is busy. Bail.
+                node = nullptr;
+                break;
+            }
+            // The node is not busy. Snatch it.
+        } while (!free_list.compare_exchange_weak(node, next.ptr()));
+        
         if (!node) {
             node = new Node;
         }
 
         new (&node->value) T(std::forward<Value>(value));
-        node->queue_next.store(nullptr, std::memory_order_relaxed);
+        // Mark the node as "busy." We'll unmark it once the value is moved out
+        // and destroyed in `try_pop_front`.
+        node->next.store(TaggedPtr<Node>(nullptr, true), std::memory_order_relaxed);
 
         push_back_node(node); // the real guts of the implementation
     }
@@ -79,54 +172,67 @@ public:
     std::optional<T> try_pop_front() {
         std::optional<T> result;
 
-        Node *old_front;
+        // `before_first` always refers to a "dummy" node that either never had
+        // a value (the initial state) or whose value was moved-from and
+        // destroyed in the previous call to `try_pop_front`.
+        //
+        // The goal of `try_pop_front` is to advance `before_first` to the next
+        // node, if any, and then to move that next node's value into `result`
+        // and destroy the source value.
+        // Finally, the previously `before_first` node can be added to the free list.
+        Node *old_before_first, *new_before_first;
         do {
-            old_front = front.load();
-            if (!old_front) {
-                return result;
-            } 
-        } while (!front.compare_exchange_weak(old_front, old_front->queue_next.load()));
+            old_before_first = before_first.load();
+            new_before_first = old_before_first->next.load().ptr();
+            if (!new_before_first) {
+                return result; // empty queue
+            }
+        } while (!before_first.compare_exchange_weak(old_before_first, new_before_first));
 
-        result.emplace(std::move(old_front->value));
-    
-        old_front->value.~T();
+        // Move the return value out of `new_before_first` and destroy the
+        // empty source.
+        // Unset the "busy" bit once we've done this.
+        const auto set_busy_bit = [](Node *node, bool bit) {
+            TaggedPtr<Node> next;
+            do {
+                next = node->next.load();
+            } while (!node->next.compare_exchange_weak(next, TaggedPtr<Node>(next.ptr(), bit)));
+        };
+        result = std::move(new_before_first->value);
+        new_before_first->value.~T();
+        set_busy_bit(new_before_first, false);
+
+        // Return `old_before_first` to the free list.
         Node *old_free_list;
         do {
-            Node *old_free_list = free_list.load();
-            old_front->free_list_next = old_free_list;
-        } while (!free_list.compare_exchange_weak(old_free_list, old_front));
-        
-        back.compare_exchange_strong(old_front, nullptr);
+            old_free_list = free_list.load();
+            // Repurpose `old_before_list->next` to point to the next element
+            // in the free list, as opposed to the next element in the queue.
+            // Be sure to preserve the `bit()` value of the `TaggedPtr`,
+            // because that holds information about whether `old_before_first`
+            // is ready to be reused.
+            TaggedPtr<Node> old_next;
+            do {
+                old_next = old_before_first->next.load();
+            } while (!old_before_first->next.compare_exchange_weak(old_next, TaggedPtr<Node>(old_free_list, old_next.bit())));
+        } while (!free_list.compare_exchange_weak(old_free_list, old_before_first));
 
         return result;
     }
 
 private:
     void push_back_node(Node *node) {
-        for (;;) {
-            Node *old_back;
-             // `nullptr` value for use as "desired" value in `compare_exchange[...]`.
-            Node *null;
-            do {
-                null = nullptr;
-                old_back = back.load();
-            } while (old_back && !old_back->queue_next.compare_exchange_weak(null, node));
-            
-            // Either `old_back` is nullptr, or we successfully stored `node` into
-            // `old_back->queue_next`.
-            
-            if (!old_back) {
-                // We're pushing onto an empty queue, so then `front` will be
-                // null, too. That is, unless another call to `push_back_node`
-                // or `push_front` beat us to the punch.
-                null = nullptr;
-                if (!front.compare_exchange_strong(null, node)) {
-                    // A concurrent call to `push_back_node` or `push_front` beat us to it.
-                    continue;
-                }   
-            }
-            back.store(node);
-            break;
-        }
+        Node *old_last;
+        TaggedPtr<Node> null;
+        do {
+            old_last = last.load();
+            const TaggedPtr<Node> next = old_last->next.load();
+            null = TaggedPtr<Node>(nullptr, next.bit());
+        } while (!old_last->next.compare_exchange_weak(null, TaggedPtr<Node>(node, true)));
+
+        // `last` now has `node` as its successor, and since `last->next`
+        // is no longer `nullptr`, other calls to `push_back_node` are spinning
+        // until we load a new value into `last`.
+        last.store(node);
     }
 };
--- test.cpp
+++ test.cpp
@@ -1,17 +1,19 @@
 #include "lock_free_queue.h"
 
+#include <iostream>
 #include <string>
 #include <thread>
 #include <vector>
 
-int main() {
+void test() {
     Queue<std::string> queue;
-    const int n_threads = 32;
+    const int n_threads = 4;
     // const int rounds = 1'000'000;
-    const int rounds = 100;
+    const int rounds = 1'000;
     std::vector<std::thread> threads;
     for (int i = 0; i < n_threads; ++i) {
         threads.emplace_back([i, &queue]() {
+            std::cerr << ("Thread " + std::to_string(i) + " has started.\n");
             queue.push_back("node from thread " + std::to_string(i));
             for (int j = 0; j < rounds; ++j) {
                 std::optional<std::string> element;
@@ -20,6 +22,7 @@ int main() {
                 } while (!element);
                 queue.push_back(std::move(*element));
             }
+            std::cerr << ("Thread " + std::to_string(i) + " has finished.\n");
         });
     }
 
@@ -30,4 +33,10 @@ int main() {
     for (int i = 0; i < n_threads / 2; ++i) {
         (void)queue.try_pop_front();
     }
+}
+
+int main() {
+    std::cout << "Beginning test.\n";
+    test();
+    std::cout << "Test complete.\n";
 }
